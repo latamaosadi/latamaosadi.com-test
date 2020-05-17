3dfe0443ed51a4353133c40ee945967a5c9e5f53
Robert Latamaosadi <robert.latamaosadi@gmail.com>
2020-05-18 00:25:24 +0800

    improve layout

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -6,26 +6,28 @@ export default function Portfolio() {
   return (
     <Layout>
       <div className="px-6 py-6">
-        <h1 className="text-center text-lg md:text-2xl font-bold">
-          Portfolio
-        </h1>
+        <h1 className="text-center text-lg md:text-2xl font-bold">Portfolio</h1>
 
         <div className="mt-8">
           <div className="flex items-center">
-            <div className="text-2xl">
-              <span>Jan 20</span>
+            <div className="w-48">
+              <img src="https://res.cloudinary.com/latamaosadi-com/image/upload/c_thumb,w_200,g_face/v1589731070/Projects/Tropik%20Lombok/landing_page.png" />
             </div>
             <div className="ml-4">
-              <h3 className="flex items-center font-bold">
-                <Link href="/projects/tropik-lombok">Tropik Lombok Website</Link>{' '}
+              <h3 className="flex items-center font-bold text-xl">
+                <Link href="/projects/tropik-lombok">Tropik Lombok</Link>{' '}
                 <a
                   href="https://www.tropiklombok.com/"
-                  className="ml-2 text-blue-500 hover:text-blue-700"
+                  className="ml-2 text-blue-500 dark:text-yellow-400 hover:text-blue-700 dark-hover:text-yellow-600 transition duration-300"
+                  target="_blank"
                 >
                   <ExternalLink size={16} />
                 </a>
               </h3>
-              <p className="text-sm">Tropik Lombok company website</p>
+              <p className="text-sm mt-2">
+                Tropik Lombok villas development company website that works as a
+                marketing tool.
+              </p>
             </div>
           </div>
         </div>
