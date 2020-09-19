bc1c9d69b1746cc043a0973bc46692383163551b
Robert Latamaosadi <robert.latamaosadi@gmail.com>
2020-09-20 02:07:30 +0800

    add line truncate

--- components/Nav.js
+++ components/Nav.js
@@ -13,7 +13,7 @@ const nav = {
   fixed: 'p-6',
   main: '',
   link:
-    'flex items-center text-blue-500 font-bold hover:bg-yellow-300 hover:text-blue-700 dark:text-yellow-400 dark-hover:bg-gray-800 transition duration-300',
+    'flex items-center text-blue-500 font-bold hover:bg-yellow-300 hover:text-blue-700 dark:text-yellow-400 dark-hover:bg-gray-800 transition duration-300 text-xs sm:text-base',
 };
 
 const Nav = () => (
@@ -22,8 +22,10 @@ const Nav = () => (
       <ul className="text-sm">
         <li>
           <a className={nav.link} href="mailto:robert@latamaosadi.com">
-            <Mail />
-            <span className="ml-2">robert@latamaosadi.com</span>
+            <Mail className="flex-shrink-0" />
+            <span className="ml-2 truncate truncate">
+              robert@latamaosadi.com
+            </span>
           </a>
         </li>
         <li className="mt-6">
@@ -32,8 +34,8 @@ const Nav = () => (
             href="https://t.me/latamaosadi"
             target="_blank"
           >
-            <Send />
-            <span className="ml-2">t.me/latamaosadi</span>
+            <Send className="flex-shrink-0" />
+            <span className="ml-2 truncate">t.me/latamaosadi</span>
           </a>
         </li>
         <li className="mt-6">
@@ -42,8 +44,8 @@ const Nav = () => (
             href="https://www.github.com/latamaosadi"
             target="_blank"
           >
-            <GitHub />
-            <span className="ml-2">github.com/latamaosadi</span>
+            <GitHub className="flex-shrink-0" />
+            <span className="ml-2 truncate">github.com/latamaosadi</span>
           </a>
         </li>
         <li className="mt-6">
@@ -52,8 +54,8 @@ const Nav = () => (
             href="https://codesandbox.io/u/latamaosadi"
             target="_blank"
           >
-            <Codesandbox />
-            <span className="ml-2">codesandbox.com/u/latamaosadi</span>
+            <Codesandbox className="flex-shrink-0" />
+            <span className="ml-2 truncate">codesandbox.com/u/latamaosadi</span>
           </a>
         </li>
         <li className="mt-6">
@@ -62,8 +64,8 @@ const Nav = () => (
             href="https://www.linkedin.com/in/latamaosadi"
             target="_blank"
           >
-            <Linkedin />
-            <span className="ml-2">linkedin.com/in/latamaosadi</span>
+            <Linkedin className="flex-shrink-0" />
+            <span className="ml-2 truncate">linkedin.com/in/latamaosadi</span>
           </a>
         </li>
         <li className="mt-6">
@@ -72,8 +74,8 @@ const Nav = () => (
             href="https://www.twitter.com/RobertLata"
             target="_blank"
           >
-            <Twitter />
-            <span className="ml-2">twitter.com/RobertLata</span>
+            <Twitter className="flex-shrink-0" />
+            <span className="ml-2 truncate">twitter.com/RobertLata</span>
           </a>
         </li>
         <li className="mt-6">
@@ -82,8 +84,8 @@ const Nav = () => (
             href="https://www.facebook.com/latamaosadi"
             target="_blank"
           >
-            <Facebook />
-            <span className="ml-2">facebook.com/latamaosadi</span>
+            <Facebook className="flex-shrink-0" />
+            <span className="ml-2 truncate">facebook.com/latamaosadi</span>
           </a>
         </li>
         <li className="mt-6">
@@ -92,8 +94,8 @@ const Nav = () => (
             href="https://www.instagram.com/latamaosadi"
             target="_blank"
           >
-            <Instagram />
-            <span className="ml-2">instagram.com/latamaosadi</span>
+            <Instagram className="flex-shrink-0" />
+            <span className="ml-2 truncate">instagram.com/latamaosadi</span>
           </a>
         </li>
       </ul>
